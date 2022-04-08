900653f737ac67df32f94db30fcfc80d1946549c
Luis Cuende <me@luisivan.net>
2022-04-08 17:37:09 +0100

    No mobile menu

--- components/Layout.js
+++ components/Layout.js
@@ -22,10 +22,10 @@ export default function Layout({ children }) {
                   </a>
                 </Link>
                 <div className="mr-2 flex items-center md:hidden">
-                  <Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-sky-500">
+                  {/*<Popover.Button className="bg-white rounded-md p-2 inline-flex items-center justify-center text-gray-400 hover:text-gray-500 hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-sky-500">
                     <span className="sr-only">Open main menu</span>
                     <MenuIcon className="h-6 w-6" aria-hidden="true" />
-                  </Popover.Button>
+                  </Popover.Button>*/}
                 </div>
               </div>
             </div>
