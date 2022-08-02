b01c14e800ec75cb167d6bc92f4e072574bfa252
Luis Cuende <me@luisivan.net>
2022-08-02 17:50:01 +0100

    Rename file

---
+++ components/Avatars.js
@@ -0,0 +1,19 @@
+import { Avatar, Tooltip } from 'flowbite-react'
+
+export default function Avatars() {
+  const avatar = (
+    <Tooltip content="Tooltip content">
+      <Avatar
+        img="https://flowbite.com/docs/images/people/profile-picture-5.jpg"
+        rounded={true}
+        size="lg"
+      />
+    </Tooltip>
+  )
+  const avatars = Array(40).fill(avatar)
+  return (
+    <div className="flex flex-wrap gap-2">
+      {avatars.map((avatar) => avatar)}
+    </div>
+  )
+}
