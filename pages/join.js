2026fdeda6dd3c7989b8c748f0d2eee3544b6d2d
Luis Cuende <me@luisivan.net>
2022-08-11 16:18:10 +0100

    WIP

--- pages/join.js
+++ pages/join.js
@@ -1,24 +1,34 @@
-import { Carousel } from 'flowbite-react'
-import SolarpunkOrb from '../public/solarpunk-orb.png'
+import SolarpunkOrb from '../public/join/solarpunk-orb.png'
 import Image from 'next/image'
 import { ChevronDoubleRightIcon } from '@heroicons/react/outline'
+import { Swiper, SwiperSlide } from 'swiper/react'
+import { Pagination, Navigation } from 'swiper'
+import { serialize } from 'next-mdx-remote/serialize'
+import { MDXRemote } from 'next-mdx-remote'
 
-const CarouselItem = ({ children, image }) => {
-  return (
-    <div className="flex h-full items-center pl-16 pr-24 sm:pr-32 gap-8">
-      <div className="lg:basis-3/5 lg:block">{children}</div>
-      <div className="basis-2/5 hidden lg:block">
-        <Image src={image} layout="responsive" />
+// Import Swiper styles
+import 'swiper/css'
+import 'swiper/css/pagination'
+import 'swiper/css/navigation'
+
+const components = { a: (props) => <a target="_blank" {...props}></a> }
+
+const CarouselItem = ({ children, image }) => (
+  <div className="flex h-full items-center px-16 gap-8">
+    <div className="lg:basis-4/6 lg:block">{children}</div>
+    <div className="basis-2/6 hidden lg:block flex h-full">
+      <div className="relative h-full rounded-full overflow-hidden">
+        <Image src={image} layout="fill" objectFit="contain" />
       </div>
     </div>
-  )
-}
+  </div>
+)
 
-export default function Join({ markdown }) {
+export default function Join({ carouselItems }) {
   return (
     <div className="h-screen flex flex-col" id="carousel">
-      <div className="my-8 h-full rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-80 bg-gradient-to-r from-n3blue to-n3green text-white">
-        <Carousel
+      <div className="my-8 h-full rounded-lg border border-gray-200 bg-white shadow-md dark:border-gray-700 dark:bg-gray-80 bg-gradient-to-r from-n3blue to-n3green text-white overflow-hidden">
+        {/*<Carousel
           indicators={false}
           slide={false}
           slideInterval={1000}
@@ -28,48 +38,94 @@ export default function Join({ markdown }) {
               <ChevronDoubleRightIcon className="h-6 w-6 text-white dark:text-gray-800 sm:h-12 sm:w-12" />
             </span>
           }
+        >*/}
+        <Swiper
+          pagination={{
+            type: 'progressbar',
+          }}
+          navigation={true}
+          modules={[Pagination, Navigation]}
+          className="h-full"
         >
-          <CarouselItem image={SolarpunkOrb}>
-            <h2 className="font-display text-5xl mb-4">1.</h2>
-            <h2 className="font-display text-4xl mb-4">Become a contributor</h2>
-            <div className="text-lg">
-              You read the manifesto, it clicked.
-              <br /> You are fed up with the way traditional states run the
-              show. You see your taxes go nowhere. Now what?
-              <br />
-              <br /> Now you can contribute to building a web3-powered,
-              tax-free, inclusive community, that will one day receive
-              international recognition and build solar punk cities you’ll want
-              to live in.
-              <br />
-              <br />
-              <ul className="list-disc">
-                <li>
-                  Head over to Discord and introduce yourself to the community
-                  (majority of channels are token-gated, but you’ll learn how to
-                  unlock them in next steps)
-                </li>
-                <li>
-                  Pick up open tasks on Dework and earn $NATION. Why would you
-                  want to have any?
-                </li>
-              </ul>
-            </div>
-          </CarouselItem>
-          <CarouselItem image={SolarpunkOrb}>{markdown}</CarouselItem>
-        </Carousel>
+          {carouselItems.map((item, i) => (
+            <SwiperSlide key={i}>
+              <CarouselItem image={item.image}>
+                <div className="prose prose-lg text-white prose-h1:font-display prose-h1:text-white prose-h1:font-light prose-h1:text-5xl prose-h1:mb-4 prose-h2:font-display prose-h2:text-white prose-h2:font-normal prose-h2:text-4xl prose-h2:mb-4 prose-h2:mt-0 prose-a:text-white prose-li:my-0">
+                  <MDXRemote {...item.serialized} components={components} />
+                </div>
+              </CarouselItem>
+            </SwiperSlide>
+          ))}
+        </Swiper>
       </div>
     </div>
   )
 }
 
 export async function getStaticProps({ params }) {
-  const card1 = await fetch('http://localhost:3000/content/join/card1.md')
-  const markdown = await card1.text()
-  console.log(markdown)
+  let carouselItems = [
+    {
+      text: `
+      # 1.
+      ## Become a contributor
+      You read the manifesto, it clicked.
+
+      You are fed up with the way traditional states run the show. You see your taxes go nowhere. Now what?
+
+      Now you can contribute to building a Web3-powered,
+      tax-free, inclusive community, that will one day receive
+      international recognition and build solar punk cities you’ll
+      want to live in.
+      
+      - Head over to Discord and introduce yourself to the community
+      (majority of channels are token-gated, but you’ll learn how
+      to unlock them in next steps)
+      - Pick up open tasks on Dework and earn $NATION. Why would you want to have any?`,
+      image: SolarpunkOrb,
+    },
+    {
+      text: `
+      # 2.
+      ## Become a co-founder
+      Holding $NATION is like holding a co-founder stake in an emerging, rapidly growing tech country. It’s how nations should have always been: not parasitically taking a cut from everything you create, but sharing in the upside. 
+
+      - You can earn your $NATION by picking up open tasks on Dework, or [buy it here.](https://app.balancer.fi/#/trade/ether/0x333A4823466879eeF910A04D473505da62142069)
+      - You can earn rewards by providing liquidity to NATION / ETH pool.
+      - You can lock up your $NATION in exchange for $veNATION* - your skin-in-the-state - and for a limited time only get access to Discord channels with a balance above 0.1 $veNATION.
+
+      You’ll want to have more than 0.1 though. Wanna know why? —>`,
+      image: '/join/flag-shade.svg',
+    },
+    {
+      text: `
+      # 3.
+      ## Become a Genesis Citizen
+      You can now become a Genesis Citizen and claim your passport out of only 420 to ever be minted.*
+
+Why would you want to be a Citizen? 
+
+- Have a say in the direction we take as a nation
+- Participate in the internal economy, including paid work and the upcoming insurance circles
+- Access all Citizen-only spaces online and offline
+- Propose your own projects and get funding
+- Benefit from all Nation3 services being built now and in the future.
+- As a Genesis Citizen, get priority access to any physical locations in Nation3.
+
+A minimum of 2 $veNATION is required to mint a passport. Whenever your lock expires, the underlying $NATION is yours to keep, sell, or re-lock.
+
+**of course, we’ll create lower passport types in the future to allow the community & economy to grow*`,
+      image: '/join/passport.svg',
+    },
+  ]
+  const serializedItems = await Promise.all(
+    carouselItems.map((item) => serialize(item.text))
+  )
+  carouselItems = carouselItems.map((item, i) => ({
+    serialized: serializedItems[i],
+    ...item,
+  }))
 
   return {
-    props: { markdown },
-    revalidate: 60 * 5,
+    props: { carouselItems },
   }
 }
