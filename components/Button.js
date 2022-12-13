89615154ebd8d0cd7241259e3b382c7e885cea11
Luis Cuende <me@luisc.xyz>
2022-12-13 16:01:31 +0000

    feat(): Added UniversalLink

--- components/Button.js
+++ components/Button.js
@@ -1,27 +1,12 @@
-import Link from 'next/link'
+import UniversalLink from './UniversalLink'
 
 const classes =
   'block bg-gradient-to-r from-n3blue via-n3green to-n3green text-white px-8 py-3 text-lg rounded-xl shadow cursor-pointer transition hover:scale-95'
 
 export default function Button({ href, children }) {
-  const absolute = /^https?:\/\//i.test(href)
-
   return (
-    <>
-      {!absolute ? (
-        <Link href={href} className={classes}>
-          {children}
-        </Link>
-      ) : (
-        <a
-          className={classes}
-          href={href}
-          rel="noopener noreferrer"
-          target="_blank"
-        >
-          {children}
-        </a>
-      )}
-    </>
+    <UniversalLink className={classes} href={href}>
+      {children}
+    </UniversalLink>
   )
 }
