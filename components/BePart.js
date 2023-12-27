2194a9d1dff97b95aa80fdb104cdca2fac6e7edb
Aahna Ashina <95955389+aahna-ashina@users.noreply.github.com>
2023-12-27 15:20:06 +0800

    rephrase

--- components/BePart.js
+++ components/BePart.js
@@ -6,7 +6,7 @@ export default function BePart() {
     <div className="text-center grid justify-items-center align-items-center py-8 lg:py-32">
       <BigTitle text="Be part of Nation3" />
       <p className="max-w-md m-auto text-lg text-center dark:text-white mb-4">
-        Zero-tax, Web3-powered, solarpunk society. One that serves you, not the other way round.
+        We are building a zero-tax, Web3-powered, solarpunk society. One that serves you, not the other way round.
       </p>
       <Button href="/join#carousel">Become a citizen</Button>
     </div>
